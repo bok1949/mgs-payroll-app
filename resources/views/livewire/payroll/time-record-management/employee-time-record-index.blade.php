8f3ff8e7c3f824d15bdec7809f75e535334a07a2
bok1949 <bryan.vigilia.flores@gmail.com>
2024-07-24 00:51:45 +0800

    put lazy mode on input search in time record index

--- resources/views/livewire/payroll/time-record-management/employee-time-record-index.blade.php
+++ resources/views/livewire/payroll/time-record-management/employee-time-record-index.blade.php
@@ -38,7 +38,7 @@
                                 type="text" 
                                 class="form-control" 
                                 placeholder="Employee name..." 
-                                wire:model.debounce.3000="searchString">
+                                wire:model.lazy="searchString">
                             <div class="form-control-icon">
                                 <i class="bi bi-search"></i>
                             </div>
